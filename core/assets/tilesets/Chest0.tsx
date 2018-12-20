b8ad8fd26c431dbd39f1666aa8794f7d854c87a3
MisterFlask <ironlordbyron@gmail.com>
2018-12-19 20:04:07 -0800

    Various pngs/tilesets

---
+++ core/assets/tilesets/Chest0.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Chest0" tilewidth="16" tileheight="16" tilecount="24" columns="8">
+ <image source="../DawnLike/Items/Chest0.png" width="128" height="48"/>
+ <tile id="0">
+  <properties>
+   <property name="chest" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="chest" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="yellow_barrel" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="broken_yellow_barrel" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
