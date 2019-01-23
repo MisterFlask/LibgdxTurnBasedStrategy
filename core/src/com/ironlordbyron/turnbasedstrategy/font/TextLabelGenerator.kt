72a535c6f082a0175ba8554706ad5a4972249bf9
Aaron <ironlordbyron@gmail.com>
2019-01-22 21:01:04 -0800

    Fixed up build.gradle for new version; fixed text label generator to not require freetype

--- core/src/com/ironlordbyron/turnbasedstrategy/font/TextLabelGenerator.kt
+++ core/src/com/ironlordbyron/turnbasedstrategy/font/TextLabelGenerator.kt
@@ -2,7 +2,6 @@ package com.ironlordbyron.turnbasedstrategy.font
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.g2d.BitmapFont
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator
 import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
