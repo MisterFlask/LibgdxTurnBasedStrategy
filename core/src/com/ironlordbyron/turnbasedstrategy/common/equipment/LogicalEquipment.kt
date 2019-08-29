b988a5a3135d22c5c3d1abaec4c3845c9cd7eaf9
ironlordbyron <ironlordbyron@gmail.com>
2019-08-28 21:41:51 -0700

    Added equipment to character roster!  Soon, I will allow players to put shit into their equipment slots!  A la XCOM!

--- core/src/com/ironlordbyron/turnbasedstrategy/common/equipment/LogicalEquipment.kt
+++ core/src/com/ironlordbyron/turnbasedstrategy/common/equipment/LogicalEquipment.kt
@@ -2,8 +2,10 @@ package com.ironlordbyron.turnbasedstrategy.common.equipment
 
 import com.ironlordbyron.turnbasedstrategy.common.abilities.LogicalAbility
 import com.ironlordbyron.turnbasedstrategy.common.abilities.LogicalAbilityEffect
+import com.ironlordbyron.turnbasedstrategy.common.characterattributes.LogicalCharacterAttribute
 
 public data class LogicalEquipment(val name: String,
                                    val equipmentClass: EquipmentClass,
                                    val abilityEnabled: Collection<LogicalAbility> = listOf(),
+                                   val attributesApplied: Collection<LogicalCharacterAttribute> = listOf(),
                                    val effectToApply: LogicalAbilityEffect? = null)
