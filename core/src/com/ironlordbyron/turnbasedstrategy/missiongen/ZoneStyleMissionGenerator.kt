01af26e20ae7544b29123259f419d7e4e760e919
ironlordbyron <ironlordbyron@gmail.com>
2019-09-15 15:50:55 -0700

    Fixed non-objective organ spawning issue.

--- core/src/com/ironlordbyron/turnbasedstrategy/missiongen/ZoneStyleMissionGenerator.kt
+++ core/src/com/ironlordbyron/turnbasedstrategy/missiongen/ZoneStyleMissionGenerator.kt
@@ -5,7 +5,6 @@ import com.ironlordbyron.turnbasedstrategy.common.TileLocation
 import com.ironlordbyron.turnbasedstrategy.common.characterattributes.AdrenalGlands
 import com.ironlordbyron.turnbasedstrategy.common.characterattributes.LogicalCharacterAttribute
 import com.ironlordbyron.turnbasedstrategy.common.characterattributes.SleepingGuardian
-import com.ironlordbyron.turnbasedstrategy.common.characterattributes.attributeOperator
 import com.ironlordbyron.turnbasedstrategy.common.wrappers.BattleGoal
 import com.ironlordbyron.turnbasedstrategy.entrypoints.SpawnableUnitTemplateTags
 import com.ironlordbyron.turnbasedstrategy.entrypoints.UnitTemplateRegistrar
@@ -19,32 +18,24 @@ import com.ironlordbyron.turnbasedstrategy.tiledutils.mapgen.TileMapProvider
  */
 public class ZoneStyleMissionGenerator{
     val unitTemplateRegistrar by LazyInject(UnitTemplateRegistrar::class.java)
+    val levelAppropriateMinionGenerator by LazyInject(LevelAppropriateMinionGenerator::class.java)
 
     /**
      * Assumes we ALWAYS want a master organ and shielding organ.  Chooses randomly among other possible organs.
      */
     private fun getOrgansToBeUsedInMission(organsToUse: Int) : Collection<TacMapUnitTemplate>{
-        assert(organsToUse > 3)
         val organs = unitTemplateRegistrar.unitTemplates.filter{it.compileTimeTags.contains(SpawnableUnitTemplateTags.ORGAN)}
                 .toMutableList()
         val masterOrgan = organs.first{it.id == "MASTER_ORGAN"}
-        val shieldingOrgan = organs.first{it.id == "SHIELDING_ORGAN"}
         organs.remove(masterOrgan)
-        organs.remove(shieldingOrgan)
         organs.shuffle()
-        val chosen = organs.take(organsToUse - 2).toMutableList()
+        val chosen = organs.take(organsToUse).toMutableList()
         return chosen.map{it.spawn()}
     }
 
     private fun getMobsToBeUsedForOrganGuardianship(mobsToUse: Int): List<TacMapUnitTemplate> {
-        val mobs = unitTemplateRegistrar
-                .unitTemplates
-                .filter{!it.compileTimeTags.contains(SpawnableUnitTemplateTags.ORGAN)}
-                .filter{it.tags.isSpawnableEnemy}
-
-        val mobsDeck = mobs.repeat(3).toMutableList()
-        mobsDeck.shuffle()
-        return mobsDeck.take(mobsToUse).map{it.spawn()}
+        val mobs = levelAppropriateMinionGenerator.getGenericMinions(mobsToUse)
+        return mobs.take(mobsToUse)
     }
 
     val tiledMapProvider by LazyInject(TileMapProvider::class.java)
@@ -54,7 +45,7 @@ public class ZoneStyleMissionGenerator{
         val bespokeZones = battleGoals.flatMap{it.getRequiredZoneCreationParameters()}.toMutableList()
         var zones = tiledMapProvider.getDiscreteZones()
         zones = zones.shuffled()
-        val organQueue = getOrgansToBeUsedInMission(2).toMutableList()
+        val nonObjectiveOrganQueue = getOrgansToBeUsedInMission(2).toMutableList()
         for (zone in zones){
             val tilesInZone = zone.tiles.shuffled().toList()
 
@@ -69,7 +60,7 @@ public class ZoneStyleMissionGenerator{
             }
             // default spawning logic
             val mobs = getMobsToBeUsedForOrganGuardianship(4)
-            val organ = organQueue.pop()
+            val organ = nonObjectiveOrganQueue.pop()
             if (organ == null){
                 continue
             }
